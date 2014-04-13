5cc4317fc3bb287c4ca9f917b05b8dcd08031a13
NZAU1984 <github@nzau.ca>
2014-04-13 07:11:20 -0400

    Search results + building instructions
    -> Added permissions needed in Manifest.xml : Internet, network state,
    read/write external storage for image caching
    -> Added Universal Image Loader library in libs folder
    -> Created ViewPagers for building instructions, when top changes,
    bottom one is updated, when bottom one is clicked, top one changed to
    appropriate image
    -> Created GridView for search result, presently displays 1000 building
    images (not set images) just to test
    -> When images are loading, a progress bar (or spinner) is displayed,
    when images are loading. progress bar/spinner disappears
    -> New layouts added in res/layout
    -> Border style added in res/drawable
    -> New images added : empty PNG image, error image for unloadable
    remote images

--- src/ca/umontreal/iro/theultimatelegoproject/BuildingInstructionActivity.java
+++ src/ca/umontreal/iro/theultimatelegoproject/BuildingInstructionActivity.java
@@ -1,15 +1,43 @@
 package ca.umontreal.iro.theultimatelegoproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+
+/*
+ * TODO :
+ * -> Simplify BigBuildingGalleryAdapter / ThumbnailBuildingGalleryAdapter ? Abstract class to be extended ?
+ * -> ONE imageLoader object, not two !
+ */
 public class BuildingInstructionActivity extends Activity
 {
-	private TextView textviewBuildingInstructionsForSet;
-	private String setId;
+	private String				setId;
+	private DisplayImageOptions	options;
+	private LayoutInflater		inflater;
+	private ViewPager			viewPagerBig;
+	private ViewPager			viewPagerThumbnails;
+	private int					bigPosition	= 0;
+	private int					thumbnailPosition	= 0;
+	private ImageView[]			thumbnailImageViews;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -18,10 +46,62 @@ public class BuildingInstructionActivity extends Activity
 		setContentView(R.layout.activity_building_instruction);
 
 		Intent intent	= getIntent();
+		setId			= intent.getStringExtra("set_id");
+		inflater		= (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		options			= new DisplayImageOptions.Builder()
+			.cacheInMemory(true)
+			.imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+			.showImageOnFail(R.drawable.image_error)
+			.build();
+		viewPagerBig	= (ViewPager) findViewById(R.id.viewpager_big);
+		viewPagerThumbnails	= (ViewPager) findViewById(R.id.viewpager_thumbnail);
 
-		setId	= intent.getStringExtra("set_id");
+		viewPagerBig.setOnPageChangeListener(new OnPageChangeListener()
+		{
 
-		initiateTextViews();
+			@Override
+			public void onPageSelected(int arg0)
+			{
+				Tools.shortToast(getApplicationContext(), "Page changed : " + arg0);
+				viewPagerThumbnails.setCurrentItem(arg0);
+
+				updateThumbnailViewPager(arg0);
+
+				bigPosition	= arg0;
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2)
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0)
+			{
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+
+		String[] temp = new String[]
+				{
+				"http://www.cubiculus.com/images/54516?",
+				"http://www.cubiculus.com/images/54517?",
+				"http://www.cubiculus.com/images/54518?",
+				"http://www.cubiculus.com/images/54519?",
+				"http://www.cubiculus.com/images/54520?",
+				"http://www.cubiculus.com/images/54521?",
+				"http://www.cubiculus.com/images/54522?",
+				"http://www.cubiculus.com/images/54523?",
+				"http://www.cubiculus.com/imaxxxges/54524?",
+				"http://upload.wikimedia.org/wikipedia/commons/8/8d/Perth_skyline_at_night.jpg"
+			};
+
+		viewPagerBig.setAdapter(new BigBuildingGalleryAdapter(this, temp));
+		viewPagerThumbnails.setAdapter(new ThumbnailBuildingGalleryAdapter(this, temp));
 	}
 
 	@Override
@@ -32,11 +112,221 @@ public class BuildingInstructionActivity extends Activity
 		return true;
 	}
 
-	private void initiateTextViews()
+	protected void addBorderToThumbnail(int position)
 	{
-		textviewBuildingInstructionsForSet	= (TextView) findViewById(R.id.textview_building_instructions_for_set);
+		if(null == thumbnailImageViews[position])
+		{
+			return;
+		}
 
-		textviewBuildingInstructionsForSet.setText(String.format(getResources().getString(R.string.garbage_buildingInstructionsForSet), setId));
+		thumbnailImageViews[position].setBackgroundResource(R.drawable.background_building_instruction_gallery_thumbnail);
+	}
+
+	protected void removeBorderToThumbnail(int position)
+	{
+		if(null == thumbnailImageViews[position])
+		{
+			return;
+		}
+
+		thumbnailImageViews[position].setBackgroundColor(Color.TRANSPARENT);
+	}
+
+	protected void updateThumbnailViewPager(int position)
+	{
+		removeBorderToThumbnail(bigPosition + 1);
+		addBorderToThumbnail(position + 1);
+	}
+
+	class BigBuildingGalleryAdapter extends PagerAdapter
+	{
+		private ImageLoader	imageLoader;
+		private String[] urlImages;
+
+		BigBuildingGalleryAdapter(Context context, String[] argUrlImages)
+		{
+			imageLoader = ImageLoader.getInstance();
+			imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+			urlImages	= argUrlImages;
+		}
+		@Override
+		public int getCount()
+		{
+			return urlImages.length;
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object)
+		{
+			return view == ((View) object);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position)
+		{
+			final View page	= inflater.inflate(R.layout.layout_building_gallery_big, null);
+
+			final ImageView imgDisp	= (ImageView) page.findViewById(R.id.imageview_image);
+
+			final ProgressBar spinner	= (ProgressBar) page.findViewById(R.id.progressbar_spinner);
+
+			imageLoader.displayImage(urlImages[position], imgDisp, options, new SimpleImageLoadingListener()
+			{
+				@Override
+				public void onLoadingStarted(String imageUri, View view)
+				{
+					spinner.setVisibility(View.VISIBLE);
+				}
+
+				@Override
+				public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+				{
+					spinner.setVisibility(View.GONE);
+				}
+
+				@Override
+				public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
+				{
+					spinner.setVisibility(View.GONE);
+				}
+			});
+
+			((ViewPager) container).addView(page, 0);
+
+			return page;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object)
+		{
+			((ViewPager) container).removeView((View) object);
+		}
+
+		@Override public float getPageWidth(int position)
+		{
+			if(1 == urlImages.length)
+			{
+				return 1f;
+			}
+
+			if(2 == urlImages.length)
+			{
+				return 0.5f;
+			}
+
+			return 1f;
+		}
+	}
+
+	class ThumbnailBuildingGalleryAdapter extends PagerAdapter
+	{
+		private ImageLoader	imageLoader;
+		private String[] urlImages;
+
+		ThumbnailBuildingGalleryAdapter(Context context, String[] argUrlImages)
+		{
+			imageLoader = ImageLoader.getInstance();
+			imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+			urlImages	= argUrlImages;
+			thumbnailImageViews	= new ImageView[argUrlImages.length + 1];
+		}
+		@Override
+		public int getCount()
+		{
+			return urlImages.length + 1;
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object)
+		{
+			return view == ((View) object);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position)
+		{
+			//Tools.shortToast(getApplicationContext(), "instant.. " + position);
+			final View page	= inflater.inflate(R.layout.layout_building_gallery_thumbnail, null);
+
+			final ImageView imgDisp	= (ImageView) page.findViewById(R.id.imageview_image);
+
+			thumbnailImageViews[position]	= imgDisp;
+
+			final ProgressBar spinner	= (ProgressBar) page.findViewById(R.id.progressbar_spinner);
+
+			updateThumbnailViewPager(bigPosition);
+
+			final int finalPosition	= position;
+
+			imgDisp.setOnClickListener(new View.OnClickListener()
+			{
+
+				@Override
+				public void onClick(View v)
+				{
+					Tools.shortToast(getApplicationContext(), "image click" + finalPosition);
+
+					viewPagerBig.setCurrentItem(finalPosition - 1);
+				}
+			});
+
+			if(0 == position)
+			{
+				imgDisp.setImageResource(R.drawable.image_empty);
+
+				return page;
+			}
+
+			imageLoader.displayImage(urlImages[position - 1], imgDisp, options, new SimpleImageLoadingListener()
+			{
+				@Override
+				public void onLoadingStarted(String imageUri, View view)
+				{
+					spinner.setVisibility(View.VISIBLE);
+				}
+
+				@Override
+				public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+				{
+					spinner.setVisibility(View.GONE);
+				}
+
+				@Override
+				public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
+				{
+					spinner.setVisibility(View.GONE);
+				}
+			});
+
+			((ViewPager) container).addView(page, 0);
+
+			return page;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object)
+		{
+			//Tools.shortToast(getApplicationContext(), "destroy " + position);
+			thumbnailImageViews[position]	= null;
+
+			((ViewPager) container).removeView((View) object);
+		}
+
+		@Override public float getPageWidth(int position)
+		{
+			if(1 == urlImages.length)
+			{
+				return 1f;
+			}
+
+			if(2 == urlImages.length)
+			{
+				return 0.5f;
+			}
+
+			return 0.333333333333f;
+		}
+
 	}
 
 }
--- src/ca/umontreal/iro/theultimatelegoproject/SearchResultActivity.java
+++ src/ca/umontreal/iro/theultimatelegoproject/SearchResultActivity.java
@@ -2,25 +2,83 @@ package ca.umontreal.iro.theultimatelegoproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingProgressListener;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 
 public class SearchResultActivity extends Activity
 {
 	private Button showSet456;
 	private Button showSet123;
 	private String sqlRequest;
-
-	// TEMP !!
 	private Boolean isFav	= false;
+	protected ImageLoader imageLoader;
+
+	String[] imageUrls;
+
+	DisplayImageOptions options;
+
+	GridView listView;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_result);
+
+		imageLoader = ImageLoader.getInstance();
+		imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+
+		imageUrls = new String[1000]; //bundle.getStringArray(Extra.IMAGES);
+		for(int i = 0; i < 1000; ++i)
+    	{
+    		String j	= String.valueOf(i);
+
+    		if(i < 100)
+    		{
+    			j	= "0" + j;
+    		}
+    		if(i < 10)
+    		{
+    			j = "0" + j;
+    		}
+
+    		imageUrls[i]	= "http://www.cubiculus.com/images/54" + j;
+    	}
+
+		options = new DisplayImageOptions.Builder()
+			.resetViewBeforeLoading(true)
+			.showImageOnFail(R.drawable.image_error)
+			.cacheInMemory(true)
+			.cacheOnDisc(true)
+			.considerExifParams(true)
+			.bitmapConfig(Bitmap.Config.RGB_565)
+			.build();
+
+		listView = (GridView) findViewById(R.id.gridview_lego_sets);
+		listView.setAdapter(new ImageAdapter());
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				//startImagePagerActivity(position);
+			}
+		});
 
 		Intent intent	= getIntent();
 
@@ -81,5 +139,262 @@ public class SearchResultActivity extends Activity
 
 		startActivity(launchSetInfoActivity);
 	}
+
+	private void startImagePagerActivity(int position) {
+		/*Intent intent = new Intent(this, ImagePagerActivity.class);
+		intent.putExtra(Extra.IMAGES, imageUrls);
+		intent.putExtra(Extra.IMAGE_POSITION, position);
+		startActivity(intent);*/
+	}
+
+	public class ImageAdapter extends BaseAdapter {
+		@Override
+		public int getCount() {
+			return imageUrls.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			final ViewHolder holder;
+			View view = convertView;
+			if (view == null) {
+				view = getLayoutInflater().inflate(R.layout.layout_search_result_lego_set, parent, false);
+				view.setLayoutParams(new AbsListView.LayoutParams(listView.getColumnWidth(),listView.getColumnWidth()));
+				holder = new ViewHolder();
+				assert view != null;
+				holder.imageView = (ImageView) view.findViewById(R.id.imageview_image);
+				holder.progressBar = (ProgressBar) view.findViewById(R.id.progressbar_spinner);
+				view.setTag(holder);
+			} else {
+				holder = (ViewHolder) view.getTag();
+			}
+
+			imageLoader.displayImage(imageUrls[position], holder.imageView, options, new SimpleImageLoadingListener() {
+										 @Override
+										 public void onLoadingStarted(String imageUri, View view) {
+											 holder.progressBar.setProgress(0);
+											 holder.progressBar.setVisibility(View.VISIBLE);
+										 }
+
+										 @Override
+										 public void onLoadingFailed(String imageUri, View view,
+												 FailReason failReason) {
+											 holder.progressBar.setVisibility(View.GONE);
+										 }
+
+										 @Override
+										 public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+											 holder.progressBar.setVisibility(View.GONE);
+										 }
+									 }, new ImageLoadingProgressListener() {
+										 @Override
+										 public void onProgressUpdate(String imageUri, View view, int current,
+												 int total) {
+											 holder.progressBar.setProgress(Math.round((100.0f * current) / total));
+										 }
+									 }
+			);
+
+			return view;
+		}
+
+		class ViewHolder {
+			ImageView imageView;
+			ProgressBar progressBar;
+		}
+	}
+/*	private Button showSet456;
+	private Button showSet123;
+	private String sqlRequest;
+	private GridView gridViewLegoSets;
+	private DisplayImageOptions	options;
+	private LayoutInflater		inflater;
+
+	// TEMP !!
+	private Boolean isFav	= false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_result);
+
+		inflater		= (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		options			= new DisplayImageOptions.Builder()
+			.cacheOnDisc(true)
+			.imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+			.showImageOnFail(R.drawable.image_error)
+			.build();
+
+		Intent intent	= getIntent();
+
+		sqlRequest	= intent.getStringExtra("sql_request");
+
+		// TEMP, the SQL request sent will tell this activity what to search, no need to tell it it's showing favorites !
+		isFav	= intent.getBooleanExtra("favorites", false);
+
+		initiateButtons();
+
+		String[] zzzmThumbIds	= new String[1000];
+
+    	for(int i = 0; i < 1000; ++i)
+    	{
+    		String j	= String.valueOf(i);
+
+    		if(i < 100)
+    		{
+    			j	= "0" + j;
+    		}
+    		if(i < 10)
+    		{
+    			j = "0" + j;
+    		}
+
+    		zzzmThumbIds[i]	= "http://www.cubiculus.com/images/54" + j;
+    	}
+
+		gridViewLegoSets	= (GridView) findViewById(R.id.gridview_lego_sets);
+		gridViewLegoSets.setAdapter(new GridViewAdapter(this, zzzmThumbIds));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search_result, menu);
+		return true;
+	}
+
+	private void initiateButtons()
+	{
+		showSet123	= (Button) findViewById(R.id.button_showset123);
+		showSet456	= (Button) findViewById(R.id.button_showset456);
+
+		if(isFav)
+		{
+			showSet456.setVisibility(View.GONE);
+		}
+		else
+		{
+			showSet123.setVisibility(View.GONE);
+		}
+
+		showSet123.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				launchSetInfoActivity("123");
+			}
+		});
+
+		showSet456.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				launchSetInfoActivity("456");
+			}
+		});
+	}
+
+	private void launchSetInfoActivity(String setId)
+	{
+		Intent launchSetInfoActivity = new Intent(this, SetInfoActivity.class);
+
+		launchSetInfoActivity.putExtra("set_id", setId);
+
+		startActivity(launchSetInfoActivity);
+	}
+
+	public class GridViewAdapter extends BaseAdapter
+	{
+	    private Context mContext;
+	    private ImageLoader imageLoader;
+
+	 // references to our images
+	    private String[] mThumbIds;
+
+	    public GridViewAdapter(Context c, String[] zzz)
+	    {
+	    	mThumbIds=zzz;
+	    	imageLoader = ImageLoader.getInstance();
+			imageLoader.init(ImageLoaderConfiguration.createDefault(c));
+	        mContext = c;
+	    }
+
+	    @Override
+		public int getCount() {
+	    	//Tools.shortToast(getApplicationContext(), "getCount " + mThumbIds.length);
+	        return mThumbIds.length;
+	    }
+
+	    @Override
+		public Object getItem(int position) {
+	        return null;
+	    }
+
+	    @Override
+		public long getItemId(int position) {
+	        return position;
+	    }
+
+	    // create a new ImageView for each item referenced by the Adapter
+	    @Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+Tools.shortToast(getApplicationContext(), "Position is " + position + ", image URL is " + mThumbIds[position]);
+	    	View page;
+
+	        if (convertView == null)
+	        {
+	        	page	= inflater.inflate(R.layout.layout_search_result_lego_set, null);
+
+	        	// We must defined a COMMON height because GridView has a bug if cells have different heights !
+	        	page.setLayoutParams(new AbsListView.LayoutParams(gridViewLegoSets.getColumnWidth(),gridViewLegoSets.getColumnWidth()));
+
+		    	final ImageView imgDisp	= (ImageView) page.findViewById(R.id.imageview_image);
+
+		    	final ProgressBar spinner	= (ProgressBar) page.findViewById(R.id.progressbar_spinner);
+
+		    	imageLoader.displayImage(mThumbIds[position], imgDisp, options, new SimpleImageLoadingListener()
+				{
+					@Override
+					public void onLoadingStarted(String imageUri, View view)
+					{
+						spinner.setVisibility(View.VISIBLE);
+					}
+
+					@Override
+					public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+					{
+						spinner.setVisibility(View.GONE);
+					}
+
+					@Override
+					public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
+					{
+						spinner.setVisibility(View.GONE);
+					}
+				});
+	        } else {
+	        	//Tools.shortToast(getApplicationContext(), "page = convertView");
+	            page	= convertView;
+	        }
+
+	       // imageView.setImageResource(mThumbIds[position]);
+
+	        return page;
+	    }
+	}*/
 
 }
