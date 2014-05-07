f56c0978f50331abb4e136e644055d84d821a904
NZAU1984 <github@nzau.ca>
2014-05-07 04:57:51 -0400

    Bug fix : search results showed incorrect favorites. Fixed.

---
+++ src/ca/umontreal/iro/theultimatelegoproject/SearchResultSet.java
@@ -0,0 +1,17 @@
+package ca.umontreal.iro.theultimatelegoproject;
+
+public class SearchResultSet
+{
+	public String	id;
+	public String	imageUrl;
+	public Boolean	seen;
+	public Boolean	favorite;
+
+	SearchResultSet(String argId, String argImageUrl, String argSeen, String argFavorite)
+	{
+		id			= argId;
+		imageUrl	= argImageUrl;
+		seen		= (argSeen.equals("1") ? true : false);
+		favorite	= (argFavorite.equals("1") ? true : false);
+	}
+}
